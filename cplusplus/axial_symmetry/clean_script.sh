1f2bdbf80e4d00cf553de82dbc582668ecad49c6
ehirvijo <eero.hirvijoki@chalmers.se>
2016-02-11 16:31:21 -0500

    Added a file and script to generate a half-circle mesh, requires gmsh to be installed (sudo apt-get install gmsh). Set the boundary domains to be extracted directly from the mesh. Fixed the evaluation of the boundary condition. Added a cleanup script.

---
+++ cplusplus/axial_symmetry/clean_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# for cleaning up execute the following commands
+
+make clean
+rm -f Makefile
+
+rm -rf CMakeFiles
+rm -f cmake_install.cmake
+rm -f CMakeCache.txt
+rm -f Forms.h
+rm -f *.msh
+rm -f *.xml
+
---
+++ cplusplus/axial_symmetry/generate_fenics_mesh.sh
@@ -0,0 +1,3 @@
+gmsh -2 half_circle.geo
+
+dolfin-convert half_circle.msh half_circle.xml
