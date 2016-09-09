59ff118d0c87b9322697627e73818dd83b5b9a19
Dylan Brennan <dylanb@princeton.edu>
2016-09-09 14:58:18 -0400

    plotter update

--- cplusplus/axial_symmetry/plot_discharge.py
+++ cplusplus/axial_symmetry/plot_discharge.py
@@ -15,7 +15,7 @@ j=data[:,3]
 dt=data[1,1]-data[2,1]
 dj=np.diff(j)/dt
 
-f, axarr = plt.subplots(6,sharex=True)
+f, axarr = plt.subplots(7,sharex=True)
 axarr[0].plot(data[:,1],data[:,3],linewidth=2,label='J_{||}')
 axarr[0].plot(data[:-1,1],dj,linewidth=2,label='J_{||}')
 axarr[0].set_title("J_{||}")
@@ -29,6 +29,8 @@ axarr[4].plot(data[:,1],data[:,7],linewidth=2,label='nz')
 axarr[4].set_title("nz")
 axarr[5].plot(data[:,1],data[:,8],linewidth=2,label='ne')
 axarr[5].set_title("ne")
+axarr[6].plot(data[:,1],data[:,9],linewidth=2,label='Te')
+axarr[6].set_title("Te")
 
 plt.xlabel('t')
 plt.show()
